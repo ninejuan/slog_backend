37fe1040692d7830ab0128d4b419708202a29b18
ninejuan <ninejuan@protonmail.com>
2024-06-01 02:13:06 +0900

    add: auth controller, rollback: auth - naver, kakao strategy is removed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './utils/swagger.util';
+import { linkToDatabase } from './utils/db.util';
+
+const env = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (env.MODE == "DEV") {
+		try {
+			setupSwagger(app);
+			console.log("Swagger is enabled");
+		} catch (e) {
+			console.error(e);
+		}
+	}
+  linkToDatabase();
   await app.listen(3000);
 }
 bootstrap();
--- src/res/auth/auth.controller.ts
+++ src/res/auth/auth.controller.ts
@@ -2,9 +2,35 @@ import { ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger';
 import { Controller, Get, Res, UseFilters, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { GoogleAuthGuard } from './guards/google.guard';
+import { CallbackUserData } from './decorator/auth.decorator';
+import Auth from 'src/interface/auth.interface';
+import Token from 'src/interface/token.interface';
 
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly authService: AuthService) { }
 
+	@ApiExcludeEndpoint()
+	@Get('google/cb')
+	@UseGuards(GoogleAuthGuard)
+	async GoogleCallback(
+		@CallbackUserData() userData: any,
+		@Res() res: Response,
+	) {
+		console.log(userData);
+		console.log(res);
+	}
+
+	@Get('/hi')
+	async hi() {
+		return 'hi';
+	}
 }
+
+/**
+ * Authentication API METHOD
+ * GET : LogIn
+ * POST : LogOut
+ * DELETE : Rm ACC
+ * PATCH METHOD에서 진행할 작업을 strategy에서 진행하는 관계로 패치 메서드는 작성하지 않음.
+ */
